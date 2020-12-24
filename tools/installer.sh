39f9ed14d57d61988c3d18f726d69f02dee687f3
Shota Shimazu <hornet.live.mf@gmail.com>
2020-12-25 01:01:10 +0900

    add tool script

---
+++ tools/installer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+before_check()
+{
+    echo 'Checking environment...'
+    if [ -e ~/.pds ]; then
+        echo 'pds is already installed!'
+        exit 1
+    fi
+}
+
+
+
+# Main
+echo
+echo '############################################################'
+echo '## PDS Installer v0.0.1 Experimental                      ##'
+echo '############################################################'
+echo
+
+before_check
---
+++ tools/uninstaller.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo 'Uninstaller is not supported.'
